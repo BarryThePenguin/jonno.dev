4593c0bc2fa3397c6d0b887427c00c8f25b2f89e
Jonathan Haines <jonno.haines@gmail.com>
2018-03-02 13:03:36 +1100

    update dependencies

--- sw.js
+++ sw.js
@@ -1,39 +1,42 @@
 /* global self */
 
-const VERSION = String(Date.now());
-const URLS = [
-	'/',
-	'/9fbc700bdd6de3cb/bundle.css',
-	'/c25d021f4d136bf7/bundle.js',
-	'assets/icon.png'
-];
+const VERSION = require('./package.json').version;
+
+const URLS = process.env.FILE_LIST;
 
 // Respond with cached resources
 self.addEventListener('fetch', e => {
-	e.respondWith(self.caches.match(e.request).then(request => {
-		if (request) {
-			return request;
-		}
-		return self.fetch(e.request);
-	}));
+	e.respondWith(
+		self.caches.match(e.request).then(request => {
+			if (request) {
+				return request;
+			}
+			return self.fetch(e.request);
+		}),
+	);
 });
 
 // Register worker
 self.addEventListener('install', e => {
-	e.waitUntil(self.caches.open(VERSION).then(cache => {
-		return cache.addAll(URLS);
-	}));
+	e.waitUntil(
+		self.caches.open(VERSION).then(cache => {
+			return cache.addAll(URLS);
+		}),
+	);
 });
 
 // Remove outdated resources
 self.addEventListener('activate', e => {
-	e.waitUntil(self.caches.keys().then(keyList => {
-		return Promise.all(keyList.map((key, i) => {
-			if (keyList[i] !== VERSION) {
-				return self.caches.delete(keyList[i]);
-			}
-
-			return undefined;
-		}));
-	}));
+	e.waitUntil(
+		self.caches.keys().then(keyList => {
+			return Promise.all(
+				keyList.map((key, i) => {
+					if (keyList[i] !== VERSION) {
+						return self.caches.delete(keyList[i]);
+					}
+					return Promise.resolve();
+				}),
+			);
+		}),
+	);
 });
